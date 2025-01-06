43956c1a925e5cd478f256f82ba3152a0c542e38
Manish Chavan <manish.chavan113@gmail.com>
2025-01-06 11:34:08 +0530

    passed the parameter of current user login id and added functions for profile

--- mployis_web/app/Http/Controllers/Frontend/PageController.php
+++ mployis_web/app/Http/Controllers/Frontend/PageController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers\Frontend;
 
-use App\Http\Controllers\Controller;
+use App\Models\Auth\User;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class PageController extends Controller
 {
@@ -27,13 +28,30 @@ class PageController extends Controller
         return view('frontend.public.contact');
     }
 
-    // --------------------
+    // --------- BEGINS::AUTH FORMS -----------
     public function showCandidateSignUpPage() {
+       
         return view('frontend.candidates.auth.sign_up', ['hideHeader' => true, 'hideFooter' => true]);
     }
     
     public function showCandidateSignInPage() {
         return view('frontend.candidates.auth.sign_in', ['hideHeader' => true, 'hideFooter' => true]);
        }
-   
+       
+    public function showCandidatePasswordPage($id) {
+        $user = User::where('id', $id)->first();
+        return view('frontend.candidates.auth.sign_in_password', ['hideHeader' => true, 'hideFooter' => true, 'user' => $user]);
+    }
+    // --------- ENDS::AUTH FORMS -----------
+
+    // ------------ BEGINS::PROFILE ------------
+    public function showCandidateProfilePage($candidate_id = null) {
+        if (is_null($candidate_id)) {
+            $candidate_id = request()->candidate_id; // Retrieve from the request if not passed via the URL
+        }
+       
+        $user = User::where('id', $candidate_id)->first();
+        return view('frontend.candidates.profile.candidate_profile', ['user' => $user]);
+    }
+    // ------------ ENDS::PROFILE ------------
 }
